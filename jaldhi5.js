a93f6d9c62dfecd49739179ef6594273ce9ca5bf
shankar534 <janapareddivijayashankar@gmail.com>
2023-08-20 02:57:45 +0530

    first commit

--- jaldhi5.js
+++ jaldhi5.js
@@ -1,14 +1,17 @@
-const displaynum= document.getElementsByClassName("number")[0]
-const disaplayAll = document.getElementsByClassName("displayAll")[0]
+let displaynum= document.getElementsByClassName("number")[0]
+const disaplayAllNumbers = document.getElementsByClassName("displayAll")[0]
+
+
+
 var size=0;
 var set = new Set();
 var arr=[]
-let newarr=[]
+let newarray=[]
 
 function getRandom(minimum,maximum){
     let min=Math.floor(minimum);
     let max=Math.ceil(maximum);
-    return Math.floor(Math.random()*max-min+min);
+    return Math.floor((Math.random()*max-min)+min);
 }
 
 function isSizeIncreased(num){
@@ -30,18 +33,32 @@ function start(){
             }
             }
     }
+    fillnumbers();
 }
+
+function fillnumbers(){
+    
+    for(let i=1;i<100;i++){
+        let grid_item = document.createElement("div");
+        grid_item.classList.add("grid-item")
+        ele=document.createElement("span")
+        ele.setAttribute("id",i)
+        ele.innerText=i;
+        linebreak = document.createElement("br");
+        grid_item.appendChild(ele);
+        disaplayAllNumbers.appendChild(grid_item);
+        if(i%10 ==0){disaplayAllNumbers.appendChild(linebreak)}
+    }
+}
+
 function display(){
-    let currentnum=arr.shift()
-if(currentnum){
-    newarr.push(currentnum)
+    let currentnum = arr.shift();
+    if(currentnum){
+    newarray.push(currentnum)
     displaynum.innerHTML="<h1>"+currentnum+"</h1>";
-    ele=document.createElement("span")
-    ele.innerText=currentnum
-    //disaplayAll.innerHTML="<h1>"+newarr+"</h1>";
-    disaplayAll.appendChild(ele)
+    document.getElementById(currentnum).style.backgroundColor = "green";
 }else{
-    if(newarr.length==99){
+    if(newarray.length==99){
      clearInterval(timerstart);
      let thankyouele = document.createElement("div")
      thankyouele.innerText="game completed thankyou";
@@ -50,4 +67,4 @@ if(currentnum){
 }
 }
 start();
-var timerstart=setInterval(display,4000)
+var timerstart=setInterval(display,400)
